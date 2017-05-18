be534fee1206ecd9cc96a96309b063ac57b78ba1
linard09 <tayebwalin939@gmail.com>
2017-05-18 16:54:17 +0300

    Update date of birth.py

--- date of birth.py
+++ date of birth.py
@@ -1,4 +1,7 @@
 print('this program prints the day of ur birth')
+#NAME:TAYEBWA.LINARD
+#REG.NO.16/U/11855/PS
+#STUDENTNO:216012445
 import calendar
 dy=int(input('please insert the date you were born:'))
 mth=int(input('please insert the month you were born:'))
